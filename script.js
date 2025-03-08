9b689c864edd6839ff9587738e1a2280e03a90c8
TranKimVan <150687024+TranKimVan@users.noreply.github.com>
2025-03-08 14:30:14 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+function createHeart() {
+    const heart = document.createElement("div");
+    heart.innerHTML = "❤️";
+    heart.classList.add("heart");
+    document.body.appendChild(heart);
+
+    heart.style.left = Math.random() * 100 + "vw";
+    heart.style.animationDuration = Math.random() * 2 + 3 + "s";
+
+    setTimeout(() => {
+        heart.remove();
+    }, 5000);
+}
+
+setInterval(createHeart, 300);
